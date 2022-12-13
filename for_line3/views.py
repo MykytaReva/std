cc7ba2e354205b367417ac9cbd6c00edd69f81c4
Mykyta Reva <mykytareva@gmail.com>
2022-12-13 11:21:12 +0100

    small changes

--- for_line3/views.py
+++ for_line3/views.py
@@ -2,12 +2,12 @@ from django.views import generic
 from .models import Sales
 
 
-class SaleListView(generic.ListView):
+class DELETEVIEW(generic.DeleteView):
     model = Sales
     template_name = 'sales/main.html'
     context_object_name = 'qs'
 
 
-class SaleDetailView(generic.DetailView):
+class DELETEVIEW(generic.DeleteView):
     model = Sales
     template_name = 'sales/detail.html'
